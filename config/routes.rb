bd41ff2e993cd14b1486baff71cc2970f384ab7b
Bettenoch <bettenoch254@gmail.com>
2023-12-04 18:05:17 +0300

    set up routes.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
